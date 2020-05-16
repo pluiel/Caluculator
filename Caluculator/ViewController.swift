2e72bdb3c4fb1d48fae8cfeeb816e95f74fa5c23
pluiel <pluiel@outlook.com>
2020-05-16 00:36:49 -0700

    finish code

--- Caluculator/ViewController.swift
+++ Caluculator/ViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var label: UILabel!
+    
     var number1: Int = 0
     var number2: Int = 0
     var number3: Int = 0
     
     var ope : Int = 0
     
-    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
     super.viewDidLoad()
@@ -25,58 +26,69 @@ class ViewController: UIViewController {
     @IBAction func select0(){
         number1 = number1*10 + 0
         label.text = String(number1)
+        
     }
     @IBAction func select1(){
         number1 = number1*10 + 1
         label.text = String(number1)
+      
     }
     @IBAction func select2(){
         number1 = number1*10 + 2
         label.text = String(number1)
+        
     }
     @IBAction func select3(){
         number1 = number1*10 + 3
         label.text = String(number1)
+        
     }
     @IBAction func select4(){
         number1 = number1*10 + 4
         label.text = String(number1)
+       
     }
     @IBAction func select5(){
         number1 = number1*10 + 5
         label.text = String(number1)
+        
     }
     @IBAction func select6(){
         number1 = number1*10 + 6
         label.text = String(number1)
+        
     }
     @IBAction func select7(){
         number1 = number1*10 + 7
         label.text = String(number1)
+        
     }
     @IBAction func select8(){
         number1 = number1*10 + 8
         label.text = String(number1)
+        
     }
     @IBAction func select9(){
         number1 = number1*10 + 9
         label.text = String(number1)
+        
     }
     @IBAction func clear(){
            number1 = 0
            label.text = String(number1)
+        
     }
     @IBAction func equal(){
     if ope == 1 {
         //プラスの時
         number3 = number2 + number1
-    } else if ope == 2{
+    } else if ope == 2 {
         //マイナスの時
         number3 = number2 - number1
-    } else if ope == 3{
+    } else if ope == 3 {
         //かける時
         number3 = number2 * number1
-    } else if ope == 4{
+    } else if ope == 4 {
         //わる時
         number3 = number2 / number1
     }
